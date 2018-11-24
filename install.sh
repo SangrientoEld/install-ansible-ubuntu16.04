f9737abd3676ec6f1ac4e737ce3db8acaa08d1a0
SangrientoEld <ds.tikhonov@gmail.com>
2018-11-24 21:05:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ apt update && \
 apt install software-properties-common -y && \
 echo -ne '\n' | apt-add-repository ppa:ansible/ansible && \
 apt update && \
-apt install ansible
+apt install ansible -y
